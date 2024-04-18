8b41b8f56eb09fb11dc4b1f0db4ccedbab4ece87
Ritwik <ritwikhero@gmail.com>
2024-04-18 14:30:10 +0530

    signup route added

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,8 @@
 cosnt express = require("express");
-
+const {User} = require("../db");
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = require("../config");
 const zod = require("zod");
-
 const router = express.Router();
 
 const signupSchema = zod.object({
@@ -11,17 +12,34 @@ const signupSchema = zod.object({
     lastName : zod.string(),
 });
 
-router.post("/signup", (req,res) =>{
+router.post("/signup", async (req,res) =>{
     const body = req.body;
     const {success} = signupSchema.safeParse(req.body);
 
     if(!success){
         return res.json({
             msg : "Email already present / incorrect inputs"
-        })
+        });
     }
-    return res.json({
-        msg : "User successfully signed up"
+
+    const user = User.findOne({
+        username : body.username
     })
 
+    if(user._id){
+        return res.json({
+            msg : "Email already present / incorrect inputs"
+        });
+    }
+
+    const dbUser = await user.create(body);
+
+    const token = jwt.sign({
+        userId : user._id,
+        
+    },JWT_SECRET);
+    res.json({
+        message: "User created successfully",
+        token: token
+    })
 });
