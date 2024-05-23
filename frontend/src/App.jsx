9672c72ec3a9964e031c0cbbe1ce384b52585287
Ritwik <ritwikhero@gmail.com>
2024-05-23 23:27:18 +0530

    Signup Page started

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,22 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Signin } from "./pages/Signin";
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
 import { Signup } from "./pages/Signup";
+import { Signin } from "./pages/Signin";
 import { Dashboard } from "./pages/Dashboard";
 import { SendMoney } from "./pages/SendMoney";
 
-
-
 function App() {
-
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path = "/signup" element = {< Signup />} />
-          <Route path = "/signin" element = {< Signin />} />
-          <Route path = "/dashboard" element = {< Dashboard />} />
-          <Route path = "/signup" element = {< SendMoney />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/send" element={<SendMoney />} />
         </Routes>
       </BrowserRouter>
     </>
