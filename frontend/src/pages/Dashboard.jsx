b4b955d6b906aaaa917b8d43162a3dd476aa75a7
Ritwik <ritwikhero@gmail.com>
2024-05-29 21:36:44 +0530

    Almost done only sigin page left

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,5 +1,13 @@
-export const Dashboard = () =>{
+import { Appbar } from "../components/Appbar"
+import { Balance } from "../components/Balance"
+import { Users } from "../components/Users"
+
+export const Dashboard = () => {
     return <div>
-        
+        <Appbar />
+        <div className="m-8">
+            <Balance value={"10,000"} />
+            <Users />
+        </div>
     </div>
 }
